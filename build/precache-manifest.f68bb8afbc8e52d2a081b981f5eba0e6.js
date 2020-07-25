24edcf3252bf2327f0a37683f55b40f8800367ec
gabrielcasetta@protonmail.com <gabrielcasetta@protonmail.com>
2020-07-25 15:02:12 -0300

    commit

---
+++ build/precache-manifest.f68bb8afbc8e52d2a081b981f5eba0e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e64e25899a1079f371bf77302ddf4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d65011c414a859acdd",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "16d3ef904ec61006bdab",
+    "url": "/static/css/main.075a71a4.chunk.css"
+  },
+  {
+    "revision": "10d65011c414a859acdd",
+    "url": "/static/js/2.d7af3a3b.chunk.js"
+  },
+  {
+    "revision": "fea4e98622268b2f0cf3f0df55dd1915",
+    "url": "/static/js/2.d7af3a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d3ef904ec61006bdab",
+    "url": "/static/js/main.6cc897fc.chunk.js"
+  },
+  {
+    "revision": "3a2383183f24fb4a8cce",
+    "url": "/static/js/runtime-main.008ef387.js"
+  }
+]);
